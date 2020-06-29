74e2fafeccfbdcfa7f10fde001915cd6ff30dafd
odeane_croney <odeane_croney@yahoo.com>
2020-06-29 21:03:04 +0200

    setting up form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import Form from "./components/Form";
 
 class App extends Component {
-  render(){
-    return (
-      <form>
-        
-      </form>
-    )
+  
+  state = {
+    weight: '',
+    height: ''
+  };
+
+  onChangeHandler = e => this.setState({ [e.target.name]: e.target.value });
+  
+  
+  
+  render() {
+    return (<div>
+      <Form
+        weight={this.state.weight}
+        height={this.state.height}
+        onChangeHandler={this.onChangeHandler}
+      />
+    </div>
+    );
   }
 }
 
