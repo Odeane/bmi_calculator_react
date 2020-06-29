d9bf3758dfc7efd17fdbe2d1586dd78e209f4d90
odeane_croney <odeane_croney@yahoo.com>
2020-06-29 22:24:38 +0200

    adding submit handler to form

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,39 @@
 import React, { Component } from 'react';
 import Form from "./components/Form";
 
+import { calculateBmi } from "./helpers/bmiHelper";
+
 class App extends Component {
-  
   state = {
-    weight: '',
-    height: ''
+    weight: "",
+    height: "",
+    bmiValue: "",
+    bmiMessage: ""
   };
 
   onChangeHandler = e => this.setState({ [e.target.name]: e.target.value });
-  
-  
-  
+
+  onSubmitHandler = e => {
+    e.preventDefault();
+    const [bmiValue, bmiMessage] = calculateBmi(
+      this.state.weight,
+      this.state.height
+    );
+    this.setState({ bmiValue: bmiValue, bmiMessage: bmiMessage });
+  };
+
   render() {
-    return (<div>
-      <Form
-        weight={this.state.weight}
-        height={this.state.height}
-        onChangeHandler={this.onChangeHandler}
-      />
-    </div>
+    return (
+      <div>
+        <Form
+          weight={this.state.weight}
+          height={this.state.height}
+          onChangeHandler={this.onChangeHandler}
+          onSubmitHandler={this.onSubmitHandler}
+        />
+      </div>
     );
   }
 }
 
 export default App;
-
--- src/__tests__/Form.spec.js
+++ src/__tests__/Form.spec.js
@@ -3,24 +3,24 @@ import { shallow } from 'enzyme';
 
 import Form from '../components/Form';
 
-describe('Form', () => {
+describe("Form", () => {
   const handleChange = jest.fn();
   const handleSubmit = jest.fn();
   const wrapper = shallow(
     <Form
-      weight='90'
-      height='190'
+      weight="90"
+      height="190"
       onChangeHandler={handleChange}
-      onChangeHandler={handleSubmit}
+      onSubmitHandler={handleSubmit}
     />
   );
 
-  it('renders with weight prop', () => {
-    expect(wrapper.find('#weight').props().toEqual('90'));
+  it("renders with weight prop", () => {
+    expect(wrapper.find("#weight").props().value).toEqual("90");
   });
 
-  it('render with height prop'() => {
-    expect(wrapper.find('#height').prop().value).toEqual('190');
+  it("renders with height prop", () => {
+    expect(wrapper.find("#height").props().value).toEqual("190");
   });
 
   it("on change the onChangeHandler is being called", () => {
@@ -30,9 +30,8 @@ describe('Form', () => {
     expect(handleChange).toHaveBeenCalled();
   });
 
-it('on submit the onSubmitHandler is being called', () => {
-  wrapper.find('form').simulate('submit');
-  expect(handleSubmit).toHaveBeenCalled();
+  it("on submit the onSubmitHandler is being called", () => {
+    wrapper.find("form").simulate("submit");
+    expect(handleSubmit).toHaveBeenCalled();
   });
-
 });
